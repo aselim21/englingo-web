4c8f889382b9729e1424b883d4b60d480f068e18
Achelia Selim <achelia.s.s@gmail.com>
2021-12-12 23:03:31 +0100

    go back before bugfixin 2

--- src/scripts/room.js
+++ src/scripts/room.js
@@ -141,20 +141,18 @@ async function processAnswerWhenReady_user1() {
 //~~~~~~~~~~~refactored~~~~~~~~~~~
 async function processOfferWhenReady_user2() {
     console.log('in processOfferWhenReady_user2');
-
-    const matchInfo = await readMyMatchInfo_req();
-    const user1_offer = matchInfo.user1_offer;
-    const user2_answer = matchInfo.user2_answer;
-    if (user1_offer && !user2_answer) {
-        await createAnswerAndConnect_user2(user1_offer, updateMatchInfo_req);
-        return 0;
-    } else {
-        setTimeout(async function () {
+    setTimeout(async function () {
+        const matchInfo = await readMyMatchInfo_req();
+        const user1_offer = matchInfo.user1_offer;
+        const user2_answer = matchInfo.user2_answer;
+        if (user1_offer && !user2_answer) {
+            await createAnswerAndConnect_user2(user1_offer, updateMatchInfo_req);
+            return 0;
+        } else {
             console.log('staring processOfferWhenReady_user2 again')
             await processOfferWhenReady_user2()
-        }, 1000)
-    }
-
+        }
+    }, 1000)
     return -1;
 
 }
