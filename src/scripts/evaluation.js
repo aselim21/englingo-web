6b153bf16b80b613e0d721b7c2a2ce3d44c7d0ab
Achelia Selim <achelia.s.s@gmail.com>
2022-02-11 00:44:03 +0100

    fixed

--- src/scripts/evaluation.js
+++ src/scripts/evaluation.js
@@ -12,7 +12,7 @@ const evaluationInfo = await readMyEvaluation_req(the_evaluation_id);
 
 document.getElementById('js-score').innerHTML = evaluationInfo.score;
 
-evaluationInfo.words_evaluated.forEach(element => {
+evaluationInfo.missionWordsEvaluated.forEach(element => {
     let li_element = document.createElement('li');
     li_element.innerHTML = element.value;
     if(element.score > 0){
