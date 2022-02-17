183e66ba85074d137eb79e8cf0f3f2a533792be8
Achelia Selim <achelia.s.s@gmail.com>
2022-02-17 20:35:13 +0100

    without env variables

--- server/server.js
+++ server/server.js
@@ -4,15 +4,14 @@ const path = require('path');
 app.use(express.json());
 app.use(express.static("src"));
 const PORT = process.env.PORT || 3000;
-const serverURL_WebService = process.env.ENGLINGO_WEB_URL;
-const serverURL_MatchService = process.env.MATCHING_SERVICE_URL;
-const serverURL_MissionService = process.env.MISSION_SERVICE_URL;
-const serverURL_EvaluationService = process.env.EVALUATION_SERVICE_URL;
-const cors = require('cors');
-
+// const serverURL_WebService = process.env.ENGLINGO_WEB_URL;
+// const serverURL_MatchService = process.env.MATCHING_SERVICE_URL;
+// const serverURL_MissionService = process.env.MISSION_SERVICE_URL;
+// const serverURL_EvaluationService = process.env.EVALUATION_SERVICE_URL;
+const cors = require('cors')
 const corsOptions = {
-  origin: [serverURL_WebService, serverURL_MatchService, serverURL_MissionService, serverURL_EvaluationService]
-};
+    origin: ["https://englingo.herokuapp.com", "https://englingo-missions.herokuapp.com", "https://englingo-evaluation.herokuapp.com", "https://webrtc-englingo.herokuapp.com"]
+  };
 app.use(cors(corsOptions));
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~RESTful Service - Methods~~~~~~~~~~~~~~~~~~~~~~~~~~
