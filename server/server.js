5a35c972e91e914fc21df4a1a8619f7e79baf53d
Achelia Selim <achelia.s.s@gmail.com>
2022-02-17 19:14:24 +0100

    switch to cors framework

--- server/server.js
+++ server/server.js
@@ -10,23 +10,28 @@ const PORT = process.env.PORT || 3000;
 let fs = require('fs');
 const serverURL_EvaluationService = 'https://englingo-evaluation.herokuapp.com';
 // const serverURL_EvaluationService = 'http://localhost:3001';
+const cors = require('cors')
+const corsOptions = {
+    origin: ["https://englingo.herokuapp.com", "https://englingo-missions.herokuapp.com", "https://englingo-evaluation.herokuapp.com", "https://webrtc-englingo.herokuapp.com"]
+  };
+app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-    const corsWhitelist = [
-        'https://webrtc-englingo.herokuapp.com',
-        'http://127.0.0.1:3000',
-        'http://localhost:3000',
-        'https://englingo.herokuapp.com'
-    ];
-    if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-    }
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Access-Control-Allow-Headers, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Cookie, Set-Cookie, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, HEAD');
-    next();
-});
+// app.use((req, res, next) => {
+//     const corsWhitelist = [
+//         'https://webrtc-englingo.herokuapp.com',
+//         'http://127.0.0.1:3000',
+//         'http://localhost:3000',
+//         'https://englingo.herokuapp.com'
+//     ];
+//     if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+//         res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     }
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Access-Control-Allow-Headers, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Cookie, Set-Cookie, Authorization');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, HEAD');
+//     next();
+// });
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~RESTful Service - Methods~~~~~~~~~~~~~~~~~~~~~~~~~~
 
