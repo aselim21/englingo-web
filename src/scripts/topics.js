ace7b09a85b01e2f7d095b8f39d18e9e831cbcdf
Achelia Selim <achelia.s.s@gmail.com>
2022-02-17 20:20:15 +0100

    clean code back and solved cors

--- src/scripts/topics.js
+++ src/scripts/topics.js
@@ -1,4 +1,3 @@
-// const serverURL_rooms = 'http://localhost:3000';
 const serverURL_MatchService = 'https://webrtc-englingo.herokuapp.com';
 const headers = new Headers();
 headers.append('Content-Type', 'application/json');
@@ -9,16 +8,16 @@ headers.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS,
 
 //Create userId
 let userId = window.localStorage.userId;
-if(!userId){
-window.localStorage.setItem('userId', `englingo_user${Math.floor(Math.random() * 10000000)}`);
-userId = window.localStorage.userId;
+if (!userId) {
+    window.localStorage.setItem('userId', `englingo_user${Math.floor(Math.random() * 10000000)}`);
+    userId = window.localStorage.userId;
 }
 //Topic buttons
-const list_of_topic_btns = document.getElementById('js-topic-buttons');
+const list_of_topic_btns = document.getElementsByClassName('js-topic-button');
 
 //attach event listener to all Topic Buttons
-for (let index = 0; index < list_of_topic_btns.children.length; index++) {
-    const element = list_of_topic_btns.children[index];
+for (let index = 0; index < list_of_topic_btns.length; index++) {
+    const element = list_of_topic_btns[index];
     element.addEventListener("click", async (e) => {
         deactivateOtherButtons(e.currentTarget);
         const topic_name = e.srcElement.getAttribute('topic');
@@ -29,16 +28,18 @@ for (let index = 0; index < list_of_topic_btns.children.length; index++) {
         createParticipant_req(data).then(() => {
             findMatch(topic_name);
         });
-    }); 
+    });
 }
-function deactivateOtherButtons(the_except_btn){
-    for (let index = 0; index < list_of_topic_btns.children.length; index++) {
-        const element = list_of_topic_btns.children[index];
-        if(element != the_except_btn){
+
+function deactivateOtherButtons(the_except_btn) {
+    for (let index = 0; index < list_of_topic_btns.length; index++) {
+        const element = list_of_topic_btns[index];
+        if (element != the_except_btn) {
             element.disabled = true;
         }
     }
 }
+
 //Keep asking for a match; If there is one, then open the room
 async function findMatch(the_topic) {
     let match_id;
@@ -47,10 +48,9 @@ async function findMatch(the_topic) {
         console.log('searching...');
         setTimeout(async function () {
             await findMatch(the_topic);
-        // 5000 -> 100    
+            // 5000 -> 100    
         }, 100)
     } else {
-        //TODO: Create a Mission here - Audit 4
         window.location.assign(`/rooms/${the_topic}/${match_id}`);
     }
 }
@@ -72,4 +72,3 @@ async function readMatchID_req() {
     });
     return response.json();
 }
-
