0ef3058b35be47b51c6e08d18022891836d32984
Achelia Selim <achelia.s.s@gmail.com>
2022-02-17 14:49:20 +0100

    back to commit 778d0e77d0

--- src/scripts/evaluation.js
+++ src/scripts/evaluation.js
@@ -6,7 +6,7 @@ headers.append('Accept', 'application/json');
 headers.append("Access-Control-Allow-Credentials", "true");
 headers.append("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials, Cookie, Set-Cookie, Authorization');
 headers.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, HEAD');
-const the_evaluation_id = window.location.pathname.split('/')[2];
+const the_evaluation_id = window.location.pathname.split('/')[2]; 
 
 const evaluationInfo = await readMyEvaluation_req(the_evaluation_id);
 
@@ -15,10 +15,11 @@ document.getElementById('js-score').innerHTML = evaluationInfo.score;
 evaluationInfo.missionWordsEvaluated.forEach(element => {
     let li_element = document.createElement('li');
     li_element.innerHTML = element.value;
-    if (element.score > 0) {
+    if(element.score > 0){
         document.getElementById('js-successful-list').appendChild(li_element);
-    } else document.getElementById('js-failed-list').appendChild(li_element);
+    }else document.getElementById('js-failed-list').appendChild(li_element);
 });
+
 
 //Requests
 async function readMyEvaluation_req(the_id) {
--- src/scripts/topics.js
+++ src/scripts/topics.js
@@ -1,3 +1,4 @@
+// const serverURL_rooms = 'http://localhost:3000';
 const serverURL_MatchService = 'https://webrtc-englingo.herokuapp.com';
 const headers = new Headers();
 headers.append('Content-Type', 'application/json');
@@ -8,16 +9,16 @@ headers.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS,
 
 //Create userId
 let userId = window.localStorage.userId;
-if (!userId) {
-    window.localStorage.setItem('userId', `englingo_user${Math.floor(Math.random() * 10000000)}`);
-    userId = window.localStorage.userId;
+if(!userId){
+window.localStorage.setItem('userId', `englingo_user${Math.floor(Math.random() * 10000000)}`);
+userId = window.localStorage.userId;
 }
 //Topic buttons
-const list_of_topic_btns = document.getElementsByClassName('js-topic-button');
+const list_of_topic_btns = document.getElementById('js-topic-buttons');
 
 //attach event listener to all Topic Buttons
-for (let index = 0; index < list_of_topic_btns.length; index++) {
-    const element = list_of_topic_btns[index];
+for (let index = 0; index < list_of_topic_btns.children.length; index++) {
+    const element = list_of_topic_btns.children[index];
     element.addEventListener("click", async (e) => {
         deactivateOtherButtons(e.currentTarget);
         const topic_name = e.srcElement.getAttribute('topic');
@@ -28,18 +29,16 @@ for (let index = 0; index < list_of_topic_btns.length; index++) {
         createParticipant_req(data).then(() => {
             findMatch(topic_name);
         });
-    });
+    }); 
 }
-
-function deactivateOtherButtons(the_except_btn) {
-    for (let index = 0; index < list_of_topic_btns.length; index++) {
-        const element = list_of_topic_btns[index];
-        if (element != the_except_btn) {
+function deactivateOtherButtons(the_except_btn){
+    for (let index = 0; index < list_of_topic_btns.children.length; index++) {
+        const element = list_of_topic_btns.children[index];
+        if(element != the_except_btn){
             element.disabled = true;
         }
     }
 }
-
 //Keep asking for a match; If there is one, then open the room
 async function findMatch(the_topic) {
     let match_id;
@@ -48,9 +47,10 @@ async function findMatch(the_topic) {
         console.log('searching...');
         setTimeout(async function () {
             await findMatch(the_topic);
-            // 5000 -> 100    
+        // 5000 -> 100    
         }, 100)
     } else {
+        //TODO: Create a Mission here - Audit 4
         window.location.assign(`/rooms/${the_topic}/${match_id}`);
     }
 }
@@ -72,3 +72,4 @@ async function readMatchID_req() {
     });
     return response.json();
 }
+
